2eec14c4824b54937929a3787751fe20de41e128
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2024-10-01 23:31:17 +0530

    done

--- ClassComponent LifeCycle/src/ComponentDidMount.jsx
+++ ClassComponent LifeCycle/src/ComponentDidMount.jsx
@@ -1,13 +1,34 @@
-import  { Component } from 'react'
+import { Component } from "react";
 
-export class ComponentDidMount extends Component {
+class ComponentDidMount extends Component {
+  constructor() {
+    super();
+    console.log("constructor");
+    this.state = {
+      name: "Rajasva Raj",
+    };
+  }
+  //!---------------------------------------------
+  componentDidMount() {
+    console.log("ComponentDidMount");
+  }
+  //!---------------------------------------------
   render() {
+    console.log("render");
     return (
-      <div>
-        
-      </div>
-    )
+      <>
+        <h2>Component Did Mount Method | React Life Cycle Method</h2>
+        <h3>Hello,{this.state.name}</h3>
+        <button
+          onClick={() => {
+            this.setState({ name: "Kuch bhi" });
+          }}
+        >
+          Click Me
+        </button>
+      </>
+    );
   }
 }
 
-export default ComponentDidMount
+export default ComponentDidMount;
