41ca7154be476216606e435d482c4013cc82f543
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2024-10-04 17:48:50 +0530

    done

--- FunctionBasedComponents/src/App.jsx
+++ FunctionBasedComponents/src/App.jsx
@@ -1,15 +1,21 @@
-import UseEffectWithUseState from "./Components/UseEffects/UseEffectWithUseState"
-import UseEffectWithProps from "./Components/UseEffects/UseEffectWithProps"
-import { useState } from "react"
+import UseEffectWithUseState from "./Components/UseEffects/UseEffectWithUseState";
+import UseEffectWithProps from "./Components/UseEffects/UseEffectWithProps";
+import { useState } from "react";
+import UseEffectWithConditions from "./Components/UseEffects/UseEffectWithConditions";
 const App = () => {
-  const [count,setCount] = useState(0)
+  // const [count,setCount] = useState(0)
+  const [age, setAge] = useState(21);
+  const [count, setCount] = useState(0);
   return (
     <>
-     {/* <UseEffectWithUseState/> */}
-     {/* <UseEffectWithProps data={count}/>
+      {/* <UseEffectWithUseState/> */}
+      {/* <UseEffectWithProps data={count}/>
      <button onClick={()=>setCount(count+1)}>update</button> */}
+      <UseEffectWithConditions age={age} count={count} />
+      <button onClick={() => setAge(age + 1)}>Update Age</button>
+      <button onClick={() => setCount(count + 1)}>Update Count</button>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- FunctionBasedComponents/src/Components/UseEffects/UseEffectWithConditions.jsx
+++ FunctionBasedComponents/src/Components/UseEffects/UseEffectWithConditions.jsx
@@ -1,8 +1,47 @@
-const UseEffectWithConditions = () => {
+import { useEffect, useState } from "react"
+
+const UseEffectWithConditions = (props) => {
+    // const [age, setAge] = useState(21)
+    // const [count, setCount] = useState(0)
+    // useEffect(()=>{
+    //     console.log("UseEffect Hook With Count" ,count);
+        
+    // },[count])  
+    // useEffect(()=>{
+    //     console.log("UseEffect Hook with age" ,age);
+        
+    // },[age])
+
+    useEffect(()=>{
+            console.log("UseEffect Hook With Count" ,props.count);
+            
+        },[props.count])  
+
+        useEffect(()=>{
+            console.log("UseEffect Hook with age" ,props.age);
+            
+        },[props.age])
+
   return (
-    <div>
-        UseEffectWithConditions
-    </div>
+    <>
+        <h1>UseEffect With Conditions</h1>
+        {/* <br />
+        <h3>Age :{age}</h3>
+        <br />
+        <br />
+        <h3>Count:{count}</h3>
+        <br />
+        <br />
+        <button onClick={()=>setAge(age+1)}>Update Age</button>
+        <button onClick={()=>setCount(count+1)}>Update Count</button> */}
+         
+
+
+        <h3>Age :{props.age}</h3>
+        <br />
+        <br />
+        <h3>Count:{props.count}</h3>
+    </>
   )
 }
 
