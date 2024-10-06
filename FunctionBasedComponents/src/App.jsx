16212bedc204c5ca93e082abf0cae50b2739b2cd
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2024-10-06 09:03:43 +0530

    pureComponents done

--- FunctionBasedComponents/src/App.jsx
+++ FunctionBasedComponents/src/App.jsx
@@ -2,18 +2,20 @@ import UseEffectWithUseState from "./Components/UseEffects/UseEffectWithUseState
 import UseEffectWithProps from "./Components/UseEffects/UseEffectWithProps";
 import { useState } from "react";
 import UseEffectWithConditions from "./Components/UseEffects/UseEffectWithConditions";
+import Pure from "./Components/PureComponents/Pure";
 const App = () => {
   // const [count,setCount] = useState(0)
-  const [age, setAge] = useState(21);
-  const [count, setCount] = useState(0);
+  // const [age, setAge] = useState(20);
+  // const [count, setCount] = useState(0);
   return (
     <>
       {/* <UseEffectWithUseState/> */}
       {/* <UseEffectWithProps data={count}/>
      <button onClick={()=>setCount(count+1)}>update</button> */}
-      <UseEffectWithConditions age={age} count={count} />
+      {/* <UseEffectWithConditions age={age} count={count} />
       <button onClick={() => setAge(age + 1)}>Update Age</button>
-      <button onClick={() => setCount(count + 1)}>Update Count</button>
+      <button onClick={() => setCount(count + 1)}>Update Count</button> */}
+      <Pure/>
     </>
   );
 };
---
+++ FunctionBasedComponents/src/Components/PureComponents/Pure.jsx
@@ -0,0 +1,26 @@
+import React, { PureComponent, PureComponentComponent } from 'react'
+
+ class Pure extends PureComponent {
+     constructor(){
+    super()
+     this.state={
+        count:0
+     }
+    }
+  render() {
+    console.log("Re-Rendering");
+    
+    return (
+      <>
+        <h2>Pure Components In React</h2>
+        <h3>Count:{this.state.count}</h3>
+        <button onClick={()=>this.setState({count:1})}>Counter</button>
+      </>
+    )
+  }
+}
+
+export default Pure
+
+
+//! Bascially pure components are used in class based components  
